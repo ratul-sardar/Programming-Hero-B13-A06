c2dd4256134f30d28e164bcbb2e430dbdb2cd265
Ratul Sardar <ratulsardar4747@gmail.com>
2026-03-29 20:14:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-extrabold">he.</h1>
+      <button className="btn btn-primary">click me</button>
+    </>
+  );
+}
+
+export default App;
