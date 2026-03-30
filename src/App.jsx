3dd1331aa839e59664bfced5605811819f0b42c1
ratul-sardar <ratulsardar4747@gmail.com>
2026-03-30 20:05:53 +0600

    added almost all the common css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ function App() {
   return (
     <>
       <h1 className="font-extrabold">he.</h1>
+      <p className="">Text me nigga! Let's play valorant!</p>
       <button className="btn btn-primary">click me</button>
+      <section className="cssContainer ] bg-amber-500">hi nigga!</section>
     </>
   );
 }
