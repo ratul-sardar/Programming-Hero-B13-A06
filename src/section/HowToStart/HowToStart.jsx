6ea0f3d0cf50a93d16d718e4468a959edb196431
ratul-sardar <ratulsardar4747@gmail.com>
2026-04-01 05:53:34 +0600

    how to start section is fully done

--- src/section/HowToStart/HowToStart.jsx
+++ src/section/HowToStart/HowToStart.jsx
@@ -1,5 +1,32 @@
 import HowToStartCard from "../../components/Cards/HowToStartCard";
 
+// Card Images
+import card1 from "../../assets/user.png";
+import card2 from "../../assets/package.png";
+import card3 from "../../assets/rocket.png";
+
+// Card contents
+const cardContents = [
+  {
+    cardDescription:
+      "Sign up for free in seconds. No credit card required to get started.",
+    cardTitle: "Create Account",
+    cardImage: card1,
+  },
+  {
+    cardDescription:
+      "Browse our catalog and select the tools that fit your needs.",
+    cardTitle: "Choose Products",
+    cardImage: card2,
+  },
+  {
+    cardDescription:
+      "Download and start using your premium  tools immediately.",
+    cardTitle: "Start Creating",
+    cardImage: card3,
+  },
+];
+
 export default function HowToStart() {
   return (
     <section id="HowToStart" className=" bg-background-white">
@@ -14,9 +41,14 @@ export default function HowToStart() {
 
         {/* Card*/}
         <div className="grid gap-6 md:gap-7.5 grid-cols-1 md:grid-cols-3">
-          <HowToStartCard></HowToStartCard>
-          <HowToStartCard></HowToStartCard>
-          <HowToStartCard></HowToStartCard>
+          {cardContents.map((card, index) => (
+            <HowToStartCard
+              cardDescription={card.cardDescription}
+              cardTitle={card.cardTitle}
+              cardImage={card.cardImage}
+              cardNumber={index + 1}
+            ></HowToStartCard>
+          ))}
         </div>
       </div>
     </section>
